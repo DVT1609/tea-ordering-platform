c406913d8867fc5c9c74277a0f84e8d48aa6e784
Dao Viet Tung <daoviettung1609@gmail.com>
2025-10-03 11:30:33 +0700

    Tao file lan 2

--- services/service-auth/cmd/server/main.go
+++ services/service-auth/cmd/server/main.go
@@ -2,3 +2,17 @@
 //server/main.go để gọi hàm kết nối với các sql và kafka từ libs/db rồi điền tham số để kết nối với các sql và kafka
 //server/main.go tạo fiber lắng nghe cổng 3001 cho service-auth
 package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/DVT1609/tea-ordering-platform/services/service-auth/internal/handler"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Post("/login", handler.LoginHandler)
+	app.Post("/register", handler.RegisterHandler)
+
+	app.Listen(":3001")
+}
